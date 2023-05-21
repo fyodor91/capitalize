227c546b9c361779f54b7d3406c9f6eaa7e0c6fc
fyodor91 <fyodorermakov91@gmail.com>
2023-05-21 14:19:06 +0700

    add capitalize.py test_capitalize.py

--- package-name/capitalize.py
+++ package-name/capitalize.py
@@ -1,6 +1,6 @@
 def capitalize(text):
     if text == '':
-        return ' '
-    first_char = text[0].upper()
-    rest_substring = text[1:]
-    return f'{first_char}{rest_substring}'
+        return ''
+    first_char, *rest_chars = text
+    rest_substring = ''.join(rest_chars)
+    return f'{first_char.upper()}{rest_substring}'
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,10 +1,10 @@
 from capitalize import capitalize
 
 
-assert capitalize('hello') == 'Hello'
+if capitalize('hello!') != 'Hello!':
+    raise Exception('Функция работает неверно!')
 
-
-assert capitalize('') == ''
-
+if capitalize('') != '':
+    raise Exception('Функция работает неверно!')
 
 print('Все тесты пройдены!')
